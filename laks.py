f1b28abb5c90294b097cc4bdeb0cdf8eca5dcfa8
aninstan <89847446+aninstan@users.noreply.github.com>
2022-10-18 13:47:59 +0200

    Update
    Added change for correct conjugation

--- laks.py
+++ laks.py
@@ -2,5 +2,9 @@ laks = int(input("Hvor mange laks? "))
 i = 1
 
 while laks >= i:
-    print(i, "laks")
-    i+=1
+    if i == 1:
+        print("1 laks")
+        i+=1
+    else: 
+        print(i, "lakser")
+        i+=1
