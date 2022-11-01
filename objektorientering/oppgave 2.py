c77b471e7cd8b7f252f1166c4568e16327c3a0da
aninstan <aninstan@outlook.com>
2022-11-01 14:07:44 +0100

    class fix

--- objektorientering/oppgave 2.py
+++ objektorientering/oppgave 2.py
@@ -1,5 +1,5 @@
 class Planet:
-    def __init__(self, name, sun_distance, rings = 0):
+    def __init__(self, name, radius, sun_distance, rings = 0):
         self.name = name
         self.sun_distance = sun_distance
         self.radius = radius
@@ -9,3 +9,4 @@ mercury = Planet("Mercury", 58, 2439.7)
 venus = Planet("Venus", 108, 6051.8)
 earth = Planet("Earth", 150, 6371)
 
+print(mercury.name, venus.radius, earth.sun_distance)
