c06e86c12a21ce2ad18ac6c9cb6b6172fe60324f
aninstan <aninstan@outlook.com>
2022-11-01 14:04:09 +0100

    class and obj added

---
+++ objektorientering/oppgave 2.py
@@ -0,0 +1,11 @@
+class Planet:
+    def __init__(self, name, sunDistance, rings = 0):
+        self.name = name
+        self.sunDistance = sunDistance
+        self.radius = radius
+        self.rings = rings
+
+mercury = Planet("Mercury", 58, 2439.7)
+venus = Planet("Venus", 108, 6051.8)
+earth = Planet("Earth", 150, 6371)
+
