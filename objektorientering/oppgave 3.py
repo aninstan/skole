277732fae3ec8e332ed625bde4314919126c9a66
aninstan <aninstan@outlook.com>
2022-11-01 14:20:51 +0100

    task 3 added

---
+++ objektorientering/oppgave 3.py
@@ -0,0 +1,15 @@
+class Person:
+    """
+    class for storing information about a person
+
+    Parameters:
+        first_name (str): first name of the person
+        last_name (str): last name of the person
+        birth_year (int): year of birth of the person
+    """
+
+    def __init__(self, first_name, last_name, birth_year):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_year = birth_year
+
